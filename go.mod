b4ee90944a77bdf730575d302ad0ef99b3d86836
Daniel Bok <daniel.bok@outlook.com>
2019-04-19 17:43:30 +0800

    Changed tarring mechanism

--- go.mod
+++ go.mod
@@ -3,19 +3,26 @@ module warden
 go 1.12
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/Microsoft/go-winio v0.4.12 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v1.13.1
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.3.3 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-chi/valve v0.0.0-20170920024740-9e45288364f4
 	github.com/jinzhu/gorm v1.9.4
 	github.com/jinzhu/now v1.0.0 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mholt/archiver v3.1.1+incompatible
+	github.com/nwaples/rardecode v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
 	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.1 // indirect
 	github.com/spf13/viper v1.3.2
+	github.com/stretchr/testify v1.2.2
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	golang.org/x/net v0.0.0-20190415214537-1da14a5a36f2 // indirect
+	gopkg.in/src-d/go-git.v4 v4.11.0
 )
--- utils/tar.go
+++ utils/tar.go
@@ -1,28 +1,17 @@
 package utils
 
 import (
-	"archive/tar"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/mholt/archiver"
 	"github.com/pkg/errors"
 )
 
-type TarDirOptions struct {
-	SkipGit bool // skips archiving if item is a child of .git folder
-}
-
 // Tars a directory or file. If tarring is successful, returns the filepath
 // of the tarred directory
-func TarDir(source, target string, options *TarDirOptions) (string, error) {
-	if options == nil {
-		options = &TarDirOptions{
-			SkipGit: false,
-		}
-	}
-
+func TarDir(source, target string) (string, error) {
 	_, err := os.Stat(source)
 	if err == os.ErrNotExist {
 		return "", err
@@ -39,58 +28,20 @@ func TarDir(source, target string, options *TarDirOptions) (string, error) {
 	// replace invalid characters
 	target = strings.Replace(target, ":", "-", -1)
 
-	// create file
-	writer, err := os.Create(target)
-	if err != nil {
-		return "", errors.Wrap(err, "error creating tarfile")
-	}
-	defer writer.Close()
-
-	// set file as a tar writer
-	tarfile := tar.NewWriter(writer)
-	defer tarfile.Close()
-
+	var files []string
 	// walk through the source folder recursively and put in everything
-	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
-		relativeName := strings.TrimPrefix(path, source+"\\")
-		if options.SkipGit && strings.HasPrefix(relativeName, ".git") {
-			return nil
-		}
-
-		// return on any error
+	if err := filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		// return on non-regular files
-		if !info.Mode().IsRegular() {
-			return nil
-		}
-
-		// set file headers for each file in the tar folder
-		header, err := tar.FileInfoHeader(info, info.Name())
-		if err != nil {
-			return errors.Wrap(err, "error setting tar header")
-		}
-
-		// update the name to correctly reflect the desired destination when untaring
-		header.Name = strings.TrimPrefix(strings.Replace(path, source, "", -1), string(filepath.Separator))
-		if err := tarfile.WriteHeader(header); err != nil {
-			return err
-		}
-
-		// copy file into tar folder
-		file, err := os.Open(path)
-		if err != nil {
-			return errors.Wrapf(err, "error open file at: %s", path)
-		}
-		defer file.Close()
-
-		_, err = io.Copy(tarfile, file)
+		files = append(files, path)
 		return err
-	})
-	if err != nil {
-		return "", errors.Wrap(err, "error tarring source")
+	}); err != nil {
+		return "", errors.Wrap(err, "error encountered when walking through source")
+	}
+	if err := archiver.Archive(files, target); err != nil {
+		return "", errors.Wrap(err, "error encountered when tarring source")
 	}
 
 	return target, nil
